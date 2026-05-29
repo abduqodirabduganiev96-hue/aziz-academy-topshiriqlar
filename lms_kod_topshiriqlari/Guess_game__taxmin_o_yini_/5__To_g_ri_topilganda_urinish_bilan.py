b7cf5a0cf20e3bd535bc781e5cbf253c2cd29503
abduqodirabduganiev96-hue <abduqodirabduganiev96@gmail.com>
2026-05-29 15:47:39 +0300

    Add: 5) To‘g‘ri topilganda urinish bilan [abduqodirabduganiev96-hue]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/5__To_g_ri_topilganda_urinish_bilan.py
@@ -0,0 +1,8 @@
+yashirin_son = 4 
+urunishlar = 0
+while True:
+    taxmin = int(input())
+    urunishlar += 1
+    if taxmin == yashirin_son:
+        print(f"Correct in {urunishlar} tries")
+        break

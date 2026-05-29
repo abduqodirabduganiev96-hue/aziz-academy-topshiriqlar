163b2ed05a412ed83147f3fce06f2c872d693909
abduqodirabduganiev96-hue <abduqodirabduganiev96@gmail.com>
2026-05-29 19:33:36 +0300

    Add AI task: Kitob do'konidagi kitoblar soni

---
+++ ai_topshiriqlar/Kitob_dokonidagi_kitoblar_soni.py
@@ -0,0 +1,17 @@
+# Kitob do'konidagi kitoblar soni
+# Kurs: IT Dasturlash
+# Mavzu: 2-mavzu: Birinchi dastur, print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+n = int(input())
+kitoblar = {}
+for i in range(n):
+    kitob_nomi, janr = input().split()
+    kitoblar[kitob_nomi] = janr
+soralgan_kitob = input()
+if soralgan_kitob in kitoblar:
+    print(f"{soralgan_kitob} {kitoblar[soralgan_kitob]}")
+else:
+    print("Bunday kitob mavjud emas")

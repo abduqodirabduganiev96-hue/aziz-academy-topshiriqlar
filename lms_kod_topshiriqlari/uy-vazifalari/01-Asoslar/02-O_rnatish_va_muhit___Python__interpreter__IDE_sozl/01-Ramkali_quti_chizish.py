89323e2ecd0978b3ba7b63b83d5252cfa91e7673
abduqodirabduganiev96-hue <abduqodirabduganiev96@gmail.com>
2026-07-03 16:42:33 +0500

    Add: Ramkali quti chizish [abduqodirabduganiev96-hue]

--- lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/02-O_rnatish_va_muhit___Python__interpreter__IDE_sozl/01-Ramkali_quti_chizish.py
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/02-O_rnatish_va_muhit___Python__interpreter__IDE_sozl/01-Ramkali_quti_chizish.py
@@ -1,10 +1,3 @@
-try:
-    matn = input().strip().upper()
-    if matn:
-        uzunlik = len(matn) + 2
-        print("+" + "-" * uzunlik + "+")
-        print(f"| {matn} |")
-        print("+" + "-" * uzunlik + "+")
-except EOFError:
-    pass
-        
+print("+----------+")
+print("|  PYTHON  |")
+print("+----------+")
